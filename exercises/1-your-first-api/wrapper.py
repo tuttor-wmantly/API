5607c8f38f6abdffba931aad0df9527e6da73ad4
Thomas <thomasharvey24@gmail.com>
2015-11-26 23:21:58 -0500

    Removed query dictionary from urls

--- exercises/1-your-first-api/wrapper.py
+++ exercises/1-your-first-api/wrapper.py
@@ -2,8 +2,8 @@ import requests
 
 class Markit:
 	def __init__(self):
-		self.lookup_url = "http://dev.markitondemand.com/Api/v2/Lookup/json?input="
-		self.quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json?symbol="
+		self.lookup_url = "http://dev.markitondemand.com/Api/v2/Lookup/json"
+		self.quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json"
 
 	def company_search(self,string):
 		pass
