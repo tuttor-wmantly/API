3df4b2f042caa8ad914d1c9e1ea601aed601abfa
William Mantly <wmantly@gmail.com>
2019-07-22 21:02:09 -0400

    Update wrapper.py

--- exercises/1-your-first-api/wrapper.py
+++ exercises/1-your-first-api/wrapper.py
@@ -2,11 +2,15 @@ import requests
 
 class Markit:
 	def __init__(self):
-		self.lookup_url = "http://dev.markitondemand.com/Api/v2/Lookup/json"
-		self.quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json"
+		self.base_url = "http://dev.markitondemand.com/Api/v2/
 
-	def company_search(self,string):
+	def company_search(self, string):
 		pass
 
-	def get_quote(self,string):
+	def get_quote(self, string):
 		pass
+
+	
+if __name__ == '__main__':
+	markit = Markit()
+	markit.company_search('Netflix')
