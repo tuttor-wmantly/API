ce7eb1194698448324c958fca14e7ad928714153
William Mantly <wmantly@gmail.com>
2019-12-15 16:06:19 -0500

    Update wrapper.py

--- exercises/1-your-first-api/wrapper.py
+++ exercises/1-your-first-api/wrapper.py
@@ -1,17 +1,46 @@
 import requests
-
-class Markit:
-	def __init__(self):
-		self.base_url = "http://dev.markitondemand.com/Api/v2/
-
-	def company_search(self, string):
-		res = requests.get(self.base_url+'api_path')
-		pass
-
-	def get_quote(self, string):
-		pass
-
+ 
+class API:
+	def __init__(self, base_url=None, token=None):
+		self.base_url = base_URL or 'http://192.168.1.50:3000/'
+		self.token = token or ''
+		
+		self.headers = {
+			'auth-token': self.token
+		}
+		
+	def __process_error(self, res):
+		if res.status_code in range(200, 299):
+			return None
+		
+		if res.status_code = 401:
+			# do stuff for bad login
+			
+		return res.reason
+		
+	def get(self, path):
+		res = request.get(self.base_url+path, headers=self.headers)
+		err = self.__process_error(res)
+		
+		return res, err
 	
-if __name__ == '__main__':
-	markit = Markit()
-	markit.company_search('Netflix')
+	def post(self, path, json=None):
+		res = request.post(self.base_url+path, json=json or {})
+		err = self.__process_error(res)
+		
+		return res, err
+		
+
+	def login(self, username, password):
+		data = {
+			'username': username,
+			'password': password
+		}
+		res, err = self.post('auth/login', data)
+		
+		if err:
+			return err
+		
+		self.token = res.json().get('token')
+		return true
+
