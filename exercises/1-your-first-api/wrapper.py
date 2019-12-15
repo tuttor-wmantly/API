a1346edba1b553e879477742723792aad15eef22
William Mantly <wmantly@gmail.com>
2019-12-15 12:49:52 -0500

    Update wrapper.py

--- exercises/1-your-first-api/wrapper.py
+++ exercises/1-your-first-api/wrapper.py
@@ -5,6 +5,7 @@ class Markit:
 		self.base_url = "http://dev.markitondemand.com/Api/v2/
 
 	def company_search(self, string):
+		res = requests.get(self.base_url+'api_path')
 		pass
 
 	def get_quote(self, string):
